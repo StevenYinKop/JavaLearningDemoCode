9b12f742259ccfb130aace020c2eebd4469c9e10
CinCommon <yinzifancn@gmail.com>
2018-01-18 23:49:49 +0800

    Lambda表达式学习完善

---
+++ src/main/java/com/yinzifan/java8Demo/stream/StreamDemo01.java
@@ -0,0 +1,13 @@
+package com.yinzifan.java8Demo.stream;
+/**
+* @author Cin
+* @time 2018/01/18 22:15:24
+* Stream API
+* 	操作步骤: 
+* 		1. 创建Stream
+* 		2. 一系列流水线式的中间操作
+*       3. 终止操作
+*/
+public class StreamDemo01 {
+	
+}
--- src/main/java/com/yinzifan/utils/MailUtils.java
+++ src/main/java/com/yinzifan/utils/MailUtils.java
@@ -1,12 +1,5 @@
 package com.yinzifan.utils;
 
-import java.util.Properties;
-
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-
 /**
 * @author Cin
 * @time 2018/01/03 21:19:15
@@ -19,14 +12,14 @@ public class MailUtils {
 	 */
 	public static void sendMail(String to, String content) {
 		//1 创建连接对象
-		Properties properties = new Properties();
-		//2 创建邮件对象
-		Session session = Session.getInstance(properties, new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("root@cincommon.com", "root");
-			}			
-		});
+//		Properties properties = new Properties();
+//		//2 创建邮件对象
+//		Session session = Session.getInstance(properties, new Authenticator() {
+//			@Override
+//			protected PasswordAuthentication getPasswordAuthentication() {
+//				return new PasswordAuthentication("root@cincommon.com", "root");
+//			}			
+//		});
 		//3 发送一封邮件
 //		Transport.send(msg);
 	}
