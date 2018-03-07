8f10569144a3698d4a2ea176fec8628ddc4a4e96
CinCommon <yinzifancn@gmail.com>
2018-03-07 23:57:01 +0800

    java 8 Demo

---
+++ src/main/java/com/yinzifan/java8Demo/stream/MyClass.java
@@ -0,0 +1,9 @@
+package com.yinzifan.java8Demo.stream;
+
+public class MyClass {
+	
+	public String getName(){
+		return "嘿嘿嘿";
+	}
+
+}
---
+++ src/main/java/com/yinzifan/java8Demo/stream/MyFun.java
@@ -0,0 +1,9 @@
+package com.yinzifan.java8Demo.stream;
+
+public interface MyFun {
+	
+	default String getName(){
+		return "哈哈哈";
+	}
+
+}
