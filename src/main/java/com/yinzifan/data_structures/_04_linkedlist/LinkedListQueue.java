5a6ae164c564e53a2d0d305eb0f72227d7aeba8c
CinCommon <452495748@qq.com>
2018-08-15 00:12:06 +0800

    add LinkedListQueue & BST Demo  -- CinCommon

--- src/main/java/com/yinzifan/data_structures/_04_linkedlist/LinkedListQueue.java
+++ src/main/java/com/yinzifan/data_structures/_04_linkedlist/LinkedListQueue.java
@@ -3,6 +3,69 @@ package com.yinzifan.data_structures._04_linkedlist;
 import com.yinzifan.data_structures._03_queue.Queue;
 
 public class LinkedListQueue<T> implements Queue<T>{
+	private Node head;
+	private Node tail;
+	private int size;
+	public LinkedListQueue() {
+		head = null;
+		tail = null;
+	}
+	@Override
+	public void enqueue(T t) {
+		if(tail == null) {
+			tail = new Node(t);
+			head = tail;
+		} else {
+			tail.next = new Node(t);
+			tail = tail.next;
+		}
+		size ++;
+	}
+	@Override
+	public T dequeue() {
+		if(head == null) {
+			return null;
+		}
+		Node retNode = head;
+		head = head.next;
+		retNode.next = null;
+		if(head == null) {
+			tail = null;
+		}
+		size --;
+		return retNode.t;
+	}
+	@Override
+	public T getFront() {
+		if(head == null) {
+			return null;
+		}
+		return head.t;
+	}
+	@Override
+	public int getSize() {
+		return size;
+	}
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder res = new StringBuilder();
+		res.append("Queue: front: ");
+		
+		Node cur = head;
+		while(cur != null) {
+			res.append(cur + "->");
+			cur = cur.next;
+		}
+		res.append("NULL tail");
+		return res.toString();
+	}
+
+
 	private class Node {
 		public T t;
 		public Node next;
@@ -11,49 +74,11 @@ public class LinkedListQueue<T> implements Queue<T>{
 			this.t = t;
 			this.next = next;
 		}
-//		public Node(T t) { this(t, null); }
-		public Node() { this(null,null); }
+		public Node(T t) { this(t, null); }
+//		public Node() { this(null,null); }
 		@Override
 		public String toString() {
 			return t.toString();
 		}
 	}
-	
-	private Node head;
-	private Node tail;
-	private int size;
-	public LinkedListQueue() {
-		head = null;
-		tail = null;
-		size = 0;
-	}
-	@Override
-	public void enqueue(T t) {
-		
-	}
-	@Override
-	public T dequeue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public T getFront() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	
-	
-	
-
 }
--- src/main/java/com/yinzifan/data_structures/_05_binary_search_tree/BinarySearchTree.java
+++ src/main/java/com/yinzifan/data_structures/_05_binary_search_tree/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package com.yinzifan.data_structures._05_binary_search_tree;
 
+import java.util.function.Consumer;
+
+import com.yinzifan.data_structures._03_queue.Queue;
+import com.yinzifan.data_structures._04_linkedlist.LinkedListQueue;
+
 public class BinarySearchTree<T extends Comparable<T>> {
 	
 	private Node root;
@@ -16,16 +21,12 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 
 	public void add(T t) {
-		if(root == null) {
-			root = new Node(t);
-			size ++;
-		} else {
-			this.root = add(this.root, t);
-		}
+		this.root = add(this.root, t);
 	}
 	
 	private Node add(Node root, T t) {
 		if(root == null) {
+			size ++;
 			return new Node(t);
 		}
 		if(t.compareTo(root.t) < 0) {
@@ -34,6 +35,117 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			root.right = add(root.right, t);
 		}
 		return root;
+	}
+	
+	public boolean contains(T t) {
+		return contains(root, t);
+	}
+	
+	
+	private boolean contains(BinarySearchTree<T>.Node node, T t) {
+		if(node == null) {
+			return false;
+		}
+		if(t.equals(node.t)) {
+			return true;
+		} else if(t.compareTo(node.t) > 0) {
+			return contains(node.right, t);
+		} else {
+//		if(t.compareTo(node.t) < 0) {
+			return contains(node.left, t);
+		}
+	}
+	/**
+	 * @param consumer
+	 */
+	public void preOrder(Consumer<T> consumer) {
+		preOrder(root, consumer);
+	}
+	/**
+	 * @param node
+	 * @param consumer
+	 */
+	private void preOrder(BinarySearchTree<T>.Node node, Consumer<T> consumer) {
+		if(node == null) {
+			return;
+		}
+		consumer.accept(node.t);
+		preOrder(node.left, consumer);
+		preOrder(node.right, consumer);
+	}
+	/**
+	 * @param consumer
+	 */
+	public void inOrder(Consumer<T> consumer) {
+		inOrder(root, consumer);
+	}
+	/**
+	 * @param node
+	 * @param consumer
+	 */
+	private void inOrder(BinarySearchTree<T>.Node node, Consumer<T> consumer) {
+		if(node == null) {
+			return;
+		}
+		inOrder(node.left, consumer);
+		consumer.accept(node.t);
+		inOrder(node.right, consumer);
+	}
+	/**
+	 * @param consumer
+	 */
+	public void postOrder(Consumer<T> consumer) {
+		postOrder(root, consumer);
+	}
+	/**
+	 * @param node
+	 * @param consumer
+	 */
+	private void postOrder(BinarySearchTree<T>.Node node, Consumer<T> consumer) {
+		if(node == null) {
+			return;
+		}
+		postOrder(node.left, consumer);
+		postOrder(node.right, consumer);
+		consumer.accept(node.t);
+	}
+	
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("BST: \n");
+		return toString(root, 0, sb);
+	}
+
+
+	private String toString(BinarySearchTree<T>.Node node, int dept, StringBuilder sb) {
+		for(int i = 0; i < dept; i ++) {
+			sb.append("--");
+		}
+		if(node == null) {
+			sb.append("NULL\n");
+			return sb.toString();
+		}
+		sb.append(node.t.toString() + "\n");
+		toString(node.left, dept + 1, sb);
+		toString(node.right, dept + 1, sb);
+		return sb.toString();
+	}
+
+	public void levelOrder(Consumer<T> consumer) {
+		Queue<Node> q = new LinkedListQueue<>();
+		q.enqueue(root);
+		while(!q.isEmpty()) {
+			Node cur = q.dequeue();
+			consumer.accept(cur.t);
+			if(cur.left!=null) {
+				q.enqueue(cur.left);
+			}
+			if(cur.right!=null) {
+				q.enqueue(cur.right);
+			}
+		}
 	}
 	
 	private class Node{
