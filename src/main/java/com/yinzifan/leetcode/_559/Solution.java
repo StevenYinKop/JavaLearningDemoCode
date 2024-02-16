cd2664429079cd6ea0e8b08531f3bea03d6bf0a7
Steven.Yin <20436656+StevenYinKop@users.noreply.github.com>
2024-02-16 23:59:21 +1100

    add Leetcode 559

---
+++ src/main/java/com/yinzifan/leetcode/_559/Solution.java
@@ -0,0 +1,65 @@
+package com.yinzifan.leetcode._559;
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+import java.util.List;
+
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+        return 1 + root.children.stream().map(this::maxDepth).max(Integer::compareTo).orElse(0);
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
